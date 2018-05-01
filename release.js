78984b1712c38a06dda88bc9605e547badf2fd7f
Brendan Martin <bmartin@relaynetwork.com>
2018-05-01 09:49:48 -0400

    Updating releases. Code change for better structure

--- release.js
+++ release.js
@@ -8,16 +8,12 @@ const pathtooutput = process.argv[5];
 const ticketregex = /RR-\d+/ig;
 const tickets = [];
 const newArr = [];
+let newSt = '';
 let logFileLines;
 
-console.log(`git --git-dir ${pathtodir}/.git log ${tag1}..${tag2} > ${pathtooutput}.log`);
+console.log(`git --git-dir ${pathtodir}/.git fetch && git --git-dir ${pathtodir}/.git pull && git --git-dir ${pathtodir}/.git log ${tag1}..${tag2} > output/${pathtooutput}.log`);
 
-//
-// if (!!pathtodir || !!tag1 ||!!tag2 || !!pathtooutput) {
-//   return 0;
-// }
-
-exec(`git --git-dir ${pathtodir}/.git log ${tag1}..${tag2} > ${pathtooutput}.log`,
+exec(`git --git-dir ${pathtodir}/.git log ${tag1}..${tag2} > output/${pathtooutput}.log`,
   (err, stdout, stderr) => {
     if (err) {
       console.log(`err: ${err}`);
@@ -35,8 +31,8 @@ exec(`git --git-dir ${pathtodir}/.git log ${tag1}..${tag2} > ${pathtooutput}.log
 
 // could be promise later
 setTimeout(() => {
-  logFileLines = fs.readFileSync(`${pathtooutput}.log`, 'ascii').toString().split("\n");
-  console.log(`${pathtooutput}.log`);
+  logFileLines = fs.readFileSync(`output/${pathtooutput}.log`, 'ascii').toString().split("\n");
+  console.log(`output/${pathtooutput}.log`);
   console.log(logFileLines);
 
   logFileLines.forEach((logLine) => {
@@ -56,11 +52,19 @@ setTimeout(() => {
 
   for (let i = 0; i < newArr.length; i++) {
     newArr[i] = `\nhttps://jira.relayzone.com/browse/${newArr[i]}`;
+    newSt += `\nhttps://jira.relayzone.com/browse/${newArr[i]}`;
   }
 
   console.log(newArr);
 
-  fs.writeFile(`${pathtooutput}-jira.txt`, newArr, (err) => {
+  fs.writeFile(`output/${pathtooutput}-jira.txt`, newArr, (err) => {
+    if (err) {
+      console.log(`Error: ${err}`);
+      return;
+    }
+  });
+
+  fs.writeFile(`output/${pathtooutput}-jira-string.txt`, newSt, (err) => {
     if (err) {
       console.log(`Error: ${err}`);
       return;
--- run_all.sh
+++ run_all.sh
@@ -1,13 +1,29 @@
 #!/usr/bin/env bash
 
-node release.js ../../client-data-management-service release-A release-B client-data-management-service
-node release.js ../../config-service release-A release-B config-service
-node release.js ../../file-engine release-A release-B file-engine
-node release.js ../../portal-frontend-v2 release-A release-B portal-frontend-v2
-node release.js ../../rn-wire-v3 release-A release-B rn-wire-v3
-node release.js ../../file-engine release-A release-B file-engine
-node release.js ../../janus release-A release-B janus
-node release.js ../../ivr-service release-A release-B ivr-service
-node release.js ../../messaging-services release-A release-B messaging-services
-node release.js ../../mobile-number-analysis-service release-A release-B mobile-number-analysis-service
-node release.js ../../rn-boomerang release-A release-B rn-boomerang
+
+cd ../../client-data-management-service && git pull && git fetch --tags&& cd ../tools/tagger
+cd ../../config-service && git pull && git fetch --tags&& cd ../tools/tagger
+cd ../../rn-portal-v3 && git pull && git fetch --tags&& cd ../tools/tagger
+cd ../../rn-wire-v3 && git pull && git fetch --tags&& cd ../tools/tagger
+cd ../../file-engine && git pull && git fetch --tags&& cd ../tools/tagger
+cd ../../janus && git pull && git fetch --tags&& cd ../tools/tagger
+cd ../../ivr-service && git pull && git fetch --tags&& cd ../tools/tagger
+cd ../../rn-search-api && git pull && git fetch --tags&& cd ../tools/tagger
+cd ../../messaging-services && git pull && git fetch --tags&& cd ../tools/tagger
+cd ../../messaging-services && git pull && git fetch --tags&& cd ../tools/tagger
+cd ../../mobile-number-analysis-service && git pull && git fetch --tags&& cd ../tools/tagger
+cd ../../rn-boomerang && git pull && git fetch --tags&& cd ../tools/tagger
+
+
+node release.js ../../client-data-management-service    release-2018-04-13.0-hotfix.1  release-2018-04-27.0 client-data-management-service
+node release.js ../../config-service                    release-2018-04-13.0           release-2018-04-27.0 config-service
+node release.js ../../rn-portal-v3                release-2018-04-13.0-hotfix-6  release-2018-04-27.0 rn-portal-v3
+node release.js ../../rn-wire-v3                        release-2018-04-13.0           release-2018-04-27.0 rn-wire-v3
+node release.js ../../file-engine                       release-2018-04-13.0-hotfix.4  release-2018-04-30.0 file-engine
+node release.js ../../janus                             release-2018-04-13.0           release-2018-04-27.0 janus
+node release.js ../../ivr-service                       release-2018-04-13.0           release-2018-04-27.0 ivr-service
+node release.js ../../rn-search-api                release-2018-04-04.0           release-2018-04-27.0 journey-service
+node release.js ../../messaging-services                release-2018-04-13.0           release-2018-04-27.0 media-services
+node release.js ../../messaging-services                release-2018-04-13.0-hotfix.2  release-2018-04-27.0 messaging-services
+node release.js ../../mobile-number-analysis-service    release-2018-04-13.0           release-2018-04-27.0 mobile-number-analysis-service
+node release.js ../../rn-boomerang                      release-2018-04-13.0           release-2018-04-27.0 rn-boomerang
